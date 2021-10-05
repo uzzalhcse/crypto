ae07207f911e3a42b6bb3d3c3c795ecd8fc913ef
Uzzal Hosen <42272402+uzzalhcse@users.noreply.github.com>
2021-10-05 10:27:26 +0600

    Create xmrig.sh

---
+++ xmrig.sh
@@ -0,0 +1,4 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz
+tar -zxvf xmrig-6.12.2-linux-x64.tar.gz
+cd xmrig-6.12.2
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:D7yeUaRkkEUQyCFftXquV4DmyufPM7puvZ
